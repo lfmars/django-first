3c570835e461cda2f6df8150b44636a20f5a2757
LfMars <lfmars@gmail.com>
2018-08-30 19:11:05 +0300

    adding/testing templates

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vote', views.vote, name = 'vote'),
+    #/polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
 
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #speju cia imam  penkis paskutinius surusiuotus pagal data
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    #template = loader.get_template('polls/index.html')
+    context = {'key_of_question_list': latest_question_list,  }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
-def vote(request):
-    return HttpResponse("Vote for me plz")
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Exception as e:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'key_of_question' : question, 'key_of_id' :question_id })
+
+def results(request, question_id):
+    response = "You're looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
